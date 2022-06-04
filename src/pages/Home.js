9a6fed1624969cf73f12e2bbedfa8ba46d8064c5
bilal <bilal@gmail.com>
2022-06-04 22:08:29 +0200

    Home page + SCSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,19 @@ import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Navigation />
+      <div className="homeContent">
+        <div className="content">
+          <h1>Bilal El Imrani</h1>
+          <h2>Développeur Front-end</h2>
+          <div className="pdf">
+            <a href="./media/CV.pdf" target="_blank">
+              Télécharger CV
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
