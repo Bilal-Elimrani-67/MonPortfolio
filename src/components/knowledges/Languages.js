a9f3c3e8e812ff9554b74bae6d705b51d332c46c
bilal <bilal@gmail.com>
2022-06-05 14:02:28 +0200

    Knowledges page

---
+++ src/components/knowledges/Languages.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import ProgressBar from "./ProgressBar";
+
+class Languages extends Component {
+  state = {
+    languages: [
+      { id: 1, value: "JavaScript", xp: 1.8 },
+      { id: 2, value: "CSS", xp: 2 },
+      { id: 3, value: "PHP", xp: 0.7 },
+      { id: 3, value: "Python", xp: 0.4 },
+    ],
+    frameworks: [
+      { id: 1, value: "React", xp: 1.4 },
+      { id: 2, value: "Bootstrap", xp: 2 },
+      { id: 3, value: "SASS", xp: 1.5 },
+      { id: 3, value: "Material UI", xp: 0.5 },
+    ],
+  };
+
+  render() {
+    let { languages, frameworks } = this.state;
+
+    return (
+      <div className="languagesFrameworks">
+        <ProgressBar />
+        <ProgressBar />
+      </div>
+    );
+  }
+}
+
+export default Languages;
---
+++ src/components/knowledges/OtherSkills.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OtherSkills = () => {
+  return <div>Autres compétences</div>;
+};
+
+export default OtherSkills;
