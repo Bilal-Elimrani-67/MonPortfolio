182800497aace1dbe7dc48a502f7457814fac379
bilal <bilal@gmail.com>
2022-08-15 22:27:26 +0200

    Icons Font Awesome

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -5,25 +5,38 @@ const Experience = () => {
     <div className="diplomes">
       <h3>Diplomes</h3>
       <div className="exp-1">
-        <h4>Bachelor UX/UI Designer</h4>
-        <h5>STUDI, Soissons</h5>
+        <h4>
+          Bachelor UX/UI Designer <i class="fas fa-graduation-cap"></i>
+        </h4>
+        <h5>
+          <i className="fas fa-map-marker-alt"></i>
+          STUDI, Soissons
+        </h5>
         <li>En cours depuis Aout 2022</li>
       </div>
       <div className="exp-2">
-        <h4>Développeur Web (BAC+2 RNCP) </h4>
-        <h5>OpenClassrooms, Paris</h5>
-
+        <h4>
+          Développeur Web (BAC+2 RNCP) <i class="fas fa-graduation-cap"></i>
+        </h4>
+        <h5>
+          <i className="fas fa-map-marker-alt"></i>OpenClassrooms, Paris
+        </h5>
         <li>De Mars 2020 à Avril 2021</li>
       </div>
       <div className="exp-3">
         <h4>BTS Technico-Commercia (Niveau)</h4>
-        <h5>Lycée Henri Meck, Molsheim</h5>
-
+        <h5>
+          <i className="fas fa-map-marker-alt"></i>Lycée Henri Meck, Molsheim
+        </h5>
         <li>Septembre 2011 à Juin2013</li>
       </div>
       <div className="exp-3">
-        <h4>BAC STG</h4>
-        <h5>Lycée Henri Meck, Molsheim</h5>
+        <h4>
+          BAC STG <i class="fas fa-graduation-cap"></i>
+        </h4>
+        <h5>
+          <i className="fas fa-map-marker-alt"></i>Lycée Henri Meck, Molsheim
+        </h5>
         <li>Septembre 2011 à Juin2013</li>
       </div>
     </div>
